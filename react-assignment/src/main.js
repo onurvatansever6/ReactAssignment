e1c8a4fe8622a47b1fdc41fe556d3b1837970ef3
Onur Vatansever <onurvatansever.6@gmail.com>
2022-11-17 03:06:59 +0300

    changes

---
+++ react-assignment/src/main.js
@@ -0,0 +1,84 @@
+
+function createObject(){
+const data = require("./enterprise-attack.json");
+let jsonData = data.objects
+let array = []
+
+for(let i = 0; i < jsonData.length; i++) {
+    let name = jsonData[i].name
+    if(jsonData[i].external_references){
+        let techniqueID = jsonData[i].external_references[0].external_id
+        let isSubtechnique = jsonData[i].x_mitre_is_subtechnique
+        
+        if(jsonData[i].kill_chain_phases) {
+            for(let j = 0; j < jsonData[i].kill_chain_phases.length; j++){
+                if(isSubtechnique) {
+                    const maintechniqueID = techniqueID.split(".")[0];
+                    let index = array.findIndex((item) => item.techniqueID === maintechniqueID)
+                        
+                    if (index === -1){  //maintechniqueID techniqueID ile eşleşmiyorsa ayrica duplicate olmasini da engelliyoruz
+                        array.push({
+                            name: undefined,
+                            techniqueID: maintechniqueID,
+                            kill_chain_phases: undefined,
+                            subtechniques: [{
+                                name: name,
+                                techniqueID: techniqueID
+                            }]
+                        })
+                    } 
+                } 
+                else {
+                    let index = array.findIndex((item) => item.techniqueID === techniqueID)
+                        
+                    if (index === -1){
+                        array.push({
+                            name: name,
+                            techniqueID: techniqueID,
+                            kill_chain_phases: jsonData[i].kill_chain_phases,
+                            subtechniques: []
+                        })
+                    } 
+                    else {
+                        array[index].name = name
+                        array[index].kill_chain_phases = jsonData[i].kill_chain_phases
+                    }
+                }
+            }
+        }
+    }
+}
+
+let resultObj = {}
+
+for( let i = 0; i < array.length; i++) {
+    let name = array[i].name
+    let techniqueID = array[i].techniqueID
+    let kill_chain_phases = array[i].kill_chain_phases
+    let subtechniques = array[i].subtechniques
+    let obj = {
+        name: name,
+        techniqueID: techniqueID,
+        subtechniques: subtechniques      
+    }
+    
+    for( let j = 0; j < kill_chain_phases.length; j++) {
+        if (resultObj[kill_chain_phases[j].phase_name]){
+            resultObj[kill_chain_phases[j].phase_name].push(obj)
+        } 
+        else {
+            resultObj[kill_chain_phases[j].phase_name] = [obj]
+        }
+    }
+}
+
+console.log(typeof(resultObj))
+return resultObj
+}
+
+const myObj = createObject()
+console.log(Object.keys(myObj))
+console.log(myObj["reconnaissance"].length)
+// console.log(myObj)
+
+
--- react-assignment/src/newLayer.js
+++ react-assignment/src/newLayer.js
@@ -1,23 +1,297 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button} from 'reactstrap';
+import { createObject } from "./main.js"
+const data = require("./enterprise-attack.json");
+
+let createSubContainer = () => {
+    let data = createObject()
+    console.log(data.impact[0])
+
+    return <div className='subtechnique-child'>
+        {data[0].name}
+    </div>
+    
+}
+
+let deneme = createSubContainer()
 
 function NewLayer() {
-  return (
+    return (
       <body>
         <div className= "NewLayer">
-          <div className= "home-button">
-            <Button
-              active
-              size="lg"
-            >
-              Create New Layer
-            </Button></div> 
+            <div className="PhaseNameCards">
+            <Card
+                color="light"
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/16fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            <Card
+                color="light"
+                style={{
+                 width: '1/14fr'
+                }}
+>
+            <CardBody>
+                <CardTitle tag="h5">
+                    Command and Control
+                </CardTitle>
+                <br></br>
+                <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                >
+                    16 techniques
+                </CardSubtitle>
+                </CardBody>
+            </Card>
+            </div>
+        </div>
+        <div className="TechniquesGrid">
+        {createSubContainer()}
         </div>
       </body>
-    
-  );
+    );
 }
 
 export default NewLayer;
